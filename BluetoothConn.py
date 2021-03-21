d62f86628652aaaaddf110712818e3fe2bb54356
NR5P <orangev8zcar@gmail.com>
2021-03-21 09:35:15 -0500

    .

--- BluetoothConn.py
+++ BluetoothConn.py
@@ -1,23 +1,33 @@
-from bluepy.btle import Scanner, DefaultDelegate
 # https://raspberrypi.stackexchange.com/questions/114149/bluetooth-library-for-raspberry-pi 
 
 # https://github.com/adafruit/Adafruit_CircuitPython_BLE/blob/master/examples/ble_demo_central.py
 
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.nordic import UARTService
+import time
+
 class BluetoothConn(DefaultDelegate):
     def __init__(self):
-        DefaultDelegate.__init__(self)
+        self._ble = BLERadio()
+        self.conn = None
 
-    def handleDiscovery(self, dev, isNewDev, isNewData):
-        if isNewDev:
-            print("Discovered device", dev.addr)
-        elif isNewData:
-            print("Received new data from", dev.addr)
+    def start(self):
+        while True:
+            self.scanForDevices()
 
     def scanForDevices(self):
-        scanner = Scanner().withDelegate(ScanDelegate())
-        devices = scanner.scan(10.0)
+        if not self.conn:
+            print("scanning....")
+            for bconn in self._ble.start_scan(ProvideServicesAdvertisement, timeout=5):
+                if UARTService in bconn.services:
+                    print("found a UARTService advertisement")
+                    self.conn = self._ble.connect(adv)
+                    break
+            self._ble.stop_scan()
+        while self.conn and self.conn.connected:
+            time.sleep(1)
+            print("connected still")
 
-        for dev in devices:
-            print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
-            for (adtype, desc, value) in dev.getScanData():
-                print("  %s = %s" % (desc, value))
+        # Stop scanning whether or not we are connected.
+        ble.stop_scan()
